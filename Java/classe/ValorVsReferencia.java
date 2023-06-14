2a4f9dadcdcd52b9054efa5cf311b05ff14a503c
André Luis <aandreluis.a.a@gmail.com>
2023-06-14 00:23:16 -0300

    finalizando atividade

--- Java/classe/ValorVsReferencia.java
+++ Java/classe/ValorVsReferencia.java
@@ -12,6 +12,41 @@ public class ValorVsReferencia {
 		
 		System.out.println(a +"" + b);
 		
+		Data d1 = new Data(1, 06, 1923);
+		Data d2 = d1;
+		
+		System.out.println(d1.obterDataFormatada());
+		System.out.println(d2.obterDataFormatada());
+		
+		d2.dia = 19;
+		d2.mes = 10;
+		
+		System.out.println(d1.obterDataFormatada());
+		System.out.println(d2.obterDataFormatada());
+		
+		d1.ano = 2032;
+		
+		System.out.println(d1.obterDataFormatada());
+		System.out.println(d2.obterDataFormatada());
+		
+		voltarDiaPadrao(d1);
+		System.out.println(d1.obterDataFormatada());
+		System.out.println(d2.obterDataFormatada());
+		
+		int c = 10;
+		voltarInteiroPadrao(c);
+		System.out.println(c);
+		
+	}
+	
+	static void voltarDiaPadrao(Data d) {
+		d.dia = 1;
+		d.mes = 1;
+		d.ano = 1970;
+	}
+	
+	static void voltarInteiroPadrao(int c) {
+		c++;
 	}
 
 }
